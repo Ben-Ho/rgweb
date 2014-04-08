d77c320239e8b544176cbe7ab852a2b0a26ea9ec
Benjamin Hohenwarter <benjamin@rs33028.(none)>
2014-04-08 16:48:13 +0200

    First commit

---
+++ components/Root/Component.php
@@ -0,0 +1,21 @@
+<?php
+class Root_Component extends Kwc_Root_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+
+        $ret['generators']['box']['component']['mainMenu'] = 'Menu_Main_Component';
+        $ret['generators']['box']['component']['bottomMenu'] = 'Menu_Bottom_Component';
+        $ret['generators']['box']['component']['metaTags'] = 'Kwc_Box_MetaTagsContent_Component';
+        $ret['generators']['title']['component'] = 'Kwc_Box_TitleEditable_Component';
+
+        $ret['editComponents'] = array('title', 'metaTags');
+
+        $ret['contentWidth'] = 780;
+        $ret['contentWidthBoxSubtract'] = array(
+            'mainMenu' => 205,
+        );
+        return $ret;
+    }
+}
