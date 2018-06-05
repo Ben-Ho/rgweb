aabff7c51433cf5255bcd0e8edd3ce735028ee6a
Ben-Ho <benjamin.hohenwarter@gmail.com>
2018-06-05 10:31:24 +0000

    Fix kwf-lib path
    works now

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ $include_path .= PATH_SEPARATOR . 'models';
 $include_path .= PATH_SEPARATOR . 'components';
 $include_path .= PATH_SEPARATOR . 'app';
 set_include_path($include_path);
-require_once '../kwf-lib/Kwf/Setup.php';
+require_once 'kwf-lib/Kwf/Setup.php';
 Kwf_Setup::setUp();
 Kwf_Setup::dispatchKwc();
 Kwf_Setup::dispatchMedia();
