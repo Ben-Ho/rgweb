3261db30fa71a351e6ca5ee59e6ba170a2742abf
Benjamin Hohenwarter <bh@vivid-planet.com>
2018-07-04 14:30:22 +0200

    Update zu 4.2.

--- components/Menu/Main/Component.php
+++ components/Menu/Main/Component.php
@@ -1,9 +1,9 @@
 <?php
 class Menu_Main_Component extends Kwc_Menu_Component
 {
-    public static function getSettings()
+    public static function getSettings($param = null)
     {
-        $ret = parent::getSettings();
+        $ret = parent::getSettings($param);
         $ret['level'] = 'main';
         $ret['rootElementClass'] .= ' kwfUp-webListNone';
         $ret['generators']['subMenu'] = array(
